92a3e7f7dc746bc6f169d03b35f822429b1b8e43
Austin Nazworth <austin.nazworth@gmail.com>
2024-12-02 23:04:27 -0500

    Day 02 p2

--- day_02/main.ts
+++ day_02/main.ts
@@ -38,8 +38,46 @@ function handle_increasing(input: number[]): boolean {
   return true;
 }
 
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
+export function part_two(input: string): number {
+  const lines = input.split("\n").map((line) => line.split(" ").map(Number));
+  lines.pop();
+
+  const result = lines
+    .map((line) => {
+      if (actually_safe(line)) {
+        return true;
+      }
+      if (questionably_safe(line)) {
+        return true;
+      }
+      return false;
+    })
+    .reduce((sum, val) => (val === true ? sum + 1 : sum), 0);
+
+  return result;
+}
+
+function actually_safe(line: number[]): boolean {
+  const difference_between = [];
+
+  for (let i = 0; i < line.length - 1; i++) {
+    difference_between.push(line[i + 1] - line[i]);
+  }
+  const increasing = difference_between.every((num) => num <= 3 && num >= 1);
+  const decreasing = difference_between.every((num) => num >= -3 && num <= -1);
+
+  return increasing || decreasing;
+}
+
+function questionably_safe(line: number[]): boolean {
+  return line.some((_, index) => {
+    const line_without_index = line.filter((_, i) => i !== index);
+    return actually_safe(line_without_index);
+  });
+}
+
 if (import.meta.main) {
   const input = Deno.readTextFileSync("input.txt");
   console.log("Part one: ", part_one(input));
+  console.log("Part two: ", part_two(input));
 }
--- day_02/main_test.ts
+++ day_02/main_test.ts
@@ -1,8 +1,12 @@
 import { assertEquals } from "@std/assert";
-import { part_one } from "./main.ts";
+import { part_one, part_two } from "./main.ts";
 
-Deno.test(function addTest() {
+Deno.test(function partOneTest() {
   const input: string = Deno.readTextFileSync("example.txt");
-  console.log(input);
   assertEquals(part_one(input), 2);
+});
+
+Deno.test(function partTwoTest() {
+  const input: string = Deno.readTextFileSync("example.txt");
+  assertEquals(part_two(input), 4);
 });
